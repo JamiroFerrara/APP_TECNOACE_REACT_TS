cb7e63b42a3f47e43e175a9133f15b8c3cd9c675
JamiroFerrara <ferrarajamiro@hotmail.com>
2022-06-21 09:51:03 +0200

    Update

--- src/utils/SignalR.js
+++ src/utils/SignalR.js
@@ -1,23 +1,29 @@
-//const signalR = require('@microsoft/signalr');
-import { HubConnectionBuilder } from '@microsoft/signalr';
+const signalR = require('@microsoft/signalr');
+import { HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
 
-const http = "https";
-const port = "15191";
-const domain = "signalr.tecnoace.eu"
-const hub = "/chatHub"
-const url = http + "://" + domain + ":" + port + hub;
-const connection = new HubConnectionBuilder().withUrl(url, {}).build();
+const url = "https://signalr.tecnoace.eu:15191/chatHub";
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
 export async function InitConnection(){
+    joinRoom("Phone", "Suck it plz");
+}
+
+const joinRoom = async (user, room) => {
   try {
-    //connection.onclose(e => {
-   // });
+
+    const connection = new HubConnectionBuilder()
+      .withUrl(url, {
+      })
+      .configureLogging(LogLevel.Information)
+      .build();
+
+    connection.onclose(e => {
+    });
 
     await connection.start().then(() => {console.log("connected")});
     await connection.invoke("SendMessage", user, room );
-    //signalR.setConnection(connection);
-
   } catch (e) {
     console.log(e);
   }
 }
+
